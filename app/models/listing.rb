a2dc6c588fcd1c906516fbd0a7a81bd3ea8e63d1
malprax <kingmalprax@gmail.com>
2015-03-17 13:31:42 +0800

    fix error handle

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,7 +10,7 @@ class Listing < ActiveRecord::Base
   
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :name, :description, :price, presence: true
-  validates :price, numerically: {greater_than: 0}
+  validates :price, numericality: {greater_than: 0}
   validates_attachment_presence :image
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
   
-  get 'seller' => "listing#seller"
+  get 'seller' => "listings#seller"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

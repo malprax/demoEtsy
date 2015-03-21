960c7fae7316733dd058c0534cd97112a7f2bec7
malprax <kingmalprax@gmail.com>
2015-03-21 22:30:17 +0800

    congfig routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   end
 
   get 'pages/about'
-
   get 'pages/contact'
-  
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

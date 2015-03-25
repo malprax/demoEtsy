a8e5a2e6ad9d69e13c45bba7faca139d6ffcd0fa
malprax <kingmalprax@gmail.com>
2015-03-25 22:29:46 +0800

    finish making transfer fund to seller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -32,18 +32,19 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id
     
-    Stripe.api_key = ENV["STRIPE_API_KEY"]
-    token = params[:stripeToken]
+    if current_user.recipient.blank?    
+        Stripe.api_key = ENV["STRIPE_API_KEY"]
+        token = params[:stripeToken]
     
-    recipient = Stripe::Recipient.create(
-      :name => current.user.name,
-      :type => "individual"
-      :bank_account => token
-    )
+        recipient = Stripe::Recipient.create(
+          :name => current.user.name,
+          :type => "individual"
+          :bank_account => token
+        )
     
-    current_user.recipient = recipient.id
-    current_user.save
-
+        current_user.recipient = recipient.id
+        current_user.save
+    end
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -57,10 +57,15 @@ class OrdersController < ApplicationController
       flash[:danger] = e.message
     end
     
+    transfer = Stripe::Transfer.create(
+         :amount => (@listing.price * 95).floor,
+         :currency => "usd",
+         :recipient => @seller.recipient
+    )
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Order was successfully created.'}
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
